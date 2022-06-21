f0e6ed7a85bd74df37aff1c02ffcc0c2c4d6adfa
Vasile Dario Coman <dario.25@live.it>
2022-06-22 00:07:44 +0200

    Added graphics interface for submit ad view and created a custom style.css only for this form

--- resources/views/livewire/ad-form.blade.php
+++ resources/views/livewire/ad-form.blade.php
@@ -6,33 +6,46 @@
     @endif
     <form wire:submit.prevent="submitAd">
         @csrf
-        <div class="form-floating mb-3">
-            <label for="floatingInput">Titolo</label>
+        {{-- Input Title --}}
+        <h6 class="input-title">Titolo del tuo annuncio</h6>
+        <div class="form-floating mb-4">
             <input type="text" class="form-control" id="floatingInput" placeholder=" " wire:model="title">
+            <label for="floatingInput">Titolo</label>
             @error('title') <span class="error">{{$message}}</span> @enderror
         </div>
-        <div class="form-floating">
-            <label for="floatingPassword">Descrizione</label>
-            <input type="text" class="form-control" id="floatingPassword" placeholder=" " wire:model="description">
+        {{-- Input Details --}}
+        <h6 class="input-title">Dettagli</h6>
+        <div class="form-floating mb-4">
+            <input type="text" class="form-control" id="floatingDescription" placeholder=" " wire:model="description">
+            <label for="floatingDescription">Descrizione</label>
             @error('description') <span class="error">{{$message}}</span> @enderror
         </div>
-        <div class="form-floating">
-            <label for="floatingPassword">Prezzo</label>
-            <input type="text" class="form-control" id="floatingPassword" placeholder=" " wire:model="price">
-            @error('price') <span class="error">{{$message}}</span> @enderror
-        </div>
-        <div class="form-floating">
-            <select class="form-select" id="floatingSelectGrid" wire:model.defer="category">
+        {{-- Input Category --}}
+        <h6 class="input-title">Categoria</h6>
+        <div class="form-floating mb-4">
+            <select class="form-select" id="floatingCategories" wire:model.defer="category">
                 <option hidden selected value="{{$category = ""}}">Seleziona categoria</option>
                 @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->category}}</option>
                 @endforeach
             </select>
-            <label for="floatingSelectGrid">Categoria</label>
+            <label for="floatingCategories">Categoria</label>
             @error('category') <span class="error">{{$message}}</span> @enderror
         </div>
+        {{-- Input Image --}}
+        <div class="mb-4">
+            <input class="form-control form-control-lg" id="formFile" type="file">
+        </div>
+        {{-- Input Price --}}
+        <h6 class="input-title">Prezzo</h6>
+        <div class="form-floating mb-4">
+            <input type="text" class="form-control" id="floatingPrice" placeholder=" " wire:model="price">
+            <label for="floatingPrice">Prezzo</label>
+            @error('price') <span class="error">{{$message}}</span> @enderror
+        </div>
+        {{-- Submit Form Button --}}
         <div>
-            <button type="submit">Crea</button>
+            <button class="form-btn" type="submit">Crea Annuncio</button>
         </div>
     </form>
 </div>
