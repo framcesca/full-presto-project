c86fe04484cc9ab2a9e937ad261b0e534c164f8f
Tommaso Simone <tsimone9838@gmail.com>
2022-06-23 18:21:03 +0200

    Created revisor dashboard tables, to verify, accepted, rejected

---
+++ app/Http/Livewire/RevisorDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RevisorDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.revisor-dashboard');
+    }
+}
---
+++ resources/views/livewire/revisor-dashboard.blade.php
@@ -0,0 +1,120 @@
+<div>
+    <div class="container">
+        <div class="row margin-up">
+            <div class="col-4">
+                <p>Annunci da revisionare:</p>
+            </div>
+            <div class="col-4">
+                <p>Annunci accettati:</p>
+            </div>
+            <div class="col-4">
+                <p>Annunci rifiutati:</p>
+            </div>
+        </div>
+        {{-- tabella da revisionare --}}
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        {{-- <th scope="col">icona</th> --}}
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Utente</th>
+                        <th scope="col">Data</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        
+                        <td>
+                            <a href="">Titolo</a>
+                        </td>
+                        <td>
+                            <p>Utente</p>
+                        </td>
+                        <td>
+                            <p>Data</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    {{-- tabella annunci rifiutati --}}
+    <div class="container">
+        <div class="row">
+            <table class="table margin-up">
+                <thead>
+                    <tr>
+                        {{-- <th scope="col">icona</th> --}}
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Utente</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Annulla</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        
+                        <td>
+                            <a href="">Titolo</a>
+                        </td>
+                        <td>
+                            <p>Utente</p>
+                        </td>
+                        <td>
+                            <p>Data</p>
+                        </td>
+                        <td>
+                            <button class="btn btn-warning">Annulla</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    {{-- tabella annunci accettati --}}
+    <div class="container">
+        <div class="row">
+            <table class="table margin-up">
+                <thead>
+                    <tr>
+                        {{-- <th scope="col">icona</th> --}}
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Utente</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Annulla</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <a href="">Titolo</a>
+                        </td>
+                        <td>
+                            <p>Utente</p>
+                        </td>
+                        <td>
+                            <p>Data</p>
+                        </td>
+                        <td>
+                            <button class="btn btn-warning">Annulla</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
