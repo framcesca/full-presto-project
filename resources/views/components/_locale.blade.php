6587de8082dce2a2d8c9180fdbde06d32a57347b
Tommaso Simone <tsimone9838@gmail.com>
2022-06-24 17:57:49 +0200

    Fixed flag icons navbar

--- resources/views/components/_locale.blade.php
+++ resources/views/components/_locale.blade.php
@@ -1,6 +1,6 @@
 <form action="{{route("set_language_locale", $lang)}}" method="POST">
     @csrf
     <button type="submit" class="nav-link" style="background-color:transparent; border:none;">
-        <span class="flag-icon flag-icon-{{$nation}}"></span>
+        <span class="fi-{{$nation}} fi"></span>
     </button>
 </form>
