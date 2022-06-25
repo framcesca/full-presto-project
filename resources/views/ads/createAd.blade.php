dfc497415e69f0a519674b023c05edfa9416ac36
Vasile Dario Coman <dario.25@live.it>
2022-06-25 12:50:57 +0200

    Created ad preview in adForm view, fixed flash message, fixed ad card background-color

--- resources/views/ads/createAd.blade.php
+++ resources/views/ads/createAd.blade.php
@@ -1,18 +1,132 @@
 <x-layout>
     <x-slot name='title'>Crea annuncio</x-slot>
 
+    {{-- Container Fluid --}}
     <div class="container-fluid bg-container-fluid">
+
+        {{-- Container Margin --}}
         <div class="container">
+
+            {{-- Wrapping Container Form --}}
             <div class="container-ad-form">
-                <h1 class="text-center mb-4">Crea il tuo annuncio</h1>
+                <h1 class="text-center mb-5">Crea il tuo annuncio</h1>
+
+                {{-- Row --}}
                 <div class="row">
-                    <div class="form-box col-11 col-md-8 col-lg-5 m-auto py-5 px-2 px-md-5">
+
+                    {{-- Left Col --}}
+                    <div class="form-box col-12 col-md-6 col-lg-5 col-xl-4 ms-auto">
                         <div class="w-100">
                             <livewire:ad-form></livewire:ad-form>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+                    {{-- Right Col --}}
+                    <div class="col-12 col-md-6 col-lg-5 col-xl-4 m-auto mt-5 mt-md-auto">
+
+                        {{-- Card --}}
+                        <div class="card adCard rounded-0 m-auto">
+
+                            @if (true)
+                                {{-- Place Holder Images Card Swiper --}}
+                                <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff;" class="swiper mySwiper2">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                        </div>
+                                    </div>
+                                </div>
+                            @else
+                                {{-- User Images Card Swiper --}}
+                                <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff;" class="swiper mySwiper2">
+                                    <div class="swiper-wrapper">
+                                        @foreach ($images as $image)
+                                        <div class="swiper-slide">
+                                            <img src="{{$image}}" />
+                                        </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endif
+
+                            {{-- Card Details --}}
+                            @if (true)
+                                <span class="adCard-cat z-index-upper">Sport</span>
+                            @else
+                                <span class="adCard-cat z-index-upper">{{$category}}</span>
+                            @endif
+
+                            <div class="card-item d-flex justify-content-between">
+                                {{-- Ad Title --}}
+                                @if (true)
+                                    <small class="adCard-title">Tavola da Surf</small>
+                                @else
+                                    <small class="adCard-title">{{$title}}</small>
+                                @endif
+                                
+                                {{-- Ad Price --}}
+                                @if (true)
+                                    <small class="adCard-price text-end">267€</small>
+                                @else
+                                    <small class="adCard-price text-end">{{$price}}</small>
+                                @endif
+                            </div>
+
+                            {{-- Ad Author --}}
+                            <div class="card-item text-start">
+                                <small class="adCard-author">{{Auth::user()->name}}</small>                        
+                            </div>
+
+                        </div>
+
+                        @if (true)
+                            {{-- Place Holder Images Swiper --}}
+                            <div thumbsSlider="" class="swiper mySwiper mt-3">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                    </div>
+                                    <div class="swiper-slide">
+                                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                    </div>
+                                </div>
+                            </div>
+                        @else
+                            {{-- User Images Swiper --}}
+                            <div thumbsSlider="" class="swiper mySwiper mt-3">
+                                <div class="swiper-wrapper">
+                                    @foreach ($images as $image)
+                                    <div class="swiper-slide">
+                                        <img src="{{$image}}" />
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endif
+
+                    </div> {{-- End Right Col --}}
+
+                </div> {{-- End Row --}}
+
+            </div> {{-- End Wrapping Container Form --}}
+
+        </div> {{-- End Container Fluid --}}
+
+    </div> {{-- End Container Fluid --}}
+
 </x-layout>
