8cae62961d86f6245be2b70a7ab50a5ad9c03b9b
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-06-26 18:28:38 +0200

    Added spatie library,created ResizeImage job,created logi to crop user image to fixed dimension and save them in one folder for each ad

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
@@ -17,4 +18,25 @@ class Image extends Model
     {
         $this->belongsTo(Ad::class);
     }
+    
+    public static function getUrlByFilePath($filePath,$w=null,$h=null)
+    {
+        if(!$w && !$h)
+        {
+            return Storage::url($filePath);
+        }
+
+        $path= dirname($filePath);
+        $filename= basename($filePath);
+        $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+
+        return Storage::url($file);
+    }
+
+    public function getUrl($w=null,$h=null)
+    {
+        return Image::getUrlByFilePath($this->path, $w,$h);
+    }
+
+
 }
