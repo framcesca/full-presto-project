e5b0a4f923e201dba9a5d111b582af61edf89fec
Tommaso Simone <tsimone9838@gmail.com>
2022-06-27 23:32:38 +0200

    Modified category on state no ads view

--- resources/views/ads/categoryAds.blade.php
+++ resources/views/ads/categoryAds.blade.php
@@ -1,30 +1,30 @@
 <x-layout class="">
     <x-slot name='title'>{{$category->category}}</x-slot>
-
     
-
+    
+    
     <div class="container mt-5">
         <div class="row mt-100">
             <div class="col-12 text-center">
                 @switch(Lang::locale())
-                    @case("en")
-                        <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_en}}</h1>
-                    @break
-
-                    @case("es")
-                        <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_es}}</h1>
-                    @break
-
-                    @case("fr")
-                        <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_fr}}</h1>
-                    @break
-
-                    @default
-                        <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_it}}</h1>
-                    @break
+                @case("en")
+                <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_en}}</h1>
+                @break
+                
+                @case("es")
+                <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_es}}</h1>
+                @break
+                
+                @case("fr")
+                <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_fr}}</h1>
+                @break
+                
+                @default
+                <h1 class="display-5">{{__('ui.catadsin')}} {{$category->category_it}}</h1>
+                @break
                 @endswitch
             </div>
-                        <!-- search bar -->
+            <!-- search bar -->
             {{-- <div class="col-12 col-md-6 offset-md-3 mt-2">
                 <form action="{{route('searchCategoryAds',compact('category'))}}" method="get" role="search">
                     <div class="input-group">
@@ -46,25 +46,25 @@
                     @else
                     <img src="{{$ad->images()->first()->getUrl(300,300)}}" class="card-img rounded-0">
                     @endif
-
+                    
                     @switch(Lang::locale())
-                        @case("en")
-                            <span class="adCard-cat">{{$ad->category->category_en}}</span>
-                        @break
-
-                        @case("es")
-                            <span class="adCard-cat">{{$ad->category->category_es}}</span>
-                        @break
-
-                        @case("fr")
-                            <span class="adCard-cat">{{$ad->category->category_fr}}</span>
-                        @break
-
-                        @default
-                            <span class="adCard-cat">{{$ad->category->category_it}}</span>
-                        @break
+                    @case("en")
+                    <span class="adCard-cat">{{$ad->category->category_en}}</span>
+                    @break
+                    
+                    @case("es")
+                    <span class="adCard-cat">{{$ad->category->category_es}}</span>
+                    @break
+                    
+                    @case("fr")
+                    <span class="adCard-cat">{{$ad->category->category_fr}}</span>
+                    @break
+                    
+                    @default
+                    <span class="adCard-cat">{{$ad->category->category_it}}</span>
+                    @break
                     @endswitch
-
+                    
                     <div class="card-item d-flex justify-content-between">
                         <small class="adCard-title">{{$ad->title}}</small> 
                         <small class="adCard-price text-end">{{$ad->price}}€</small>
@@ -75,12 +75,13 @@
                 </a>
             </div>
             @empty
-            <div>
-            <h2>{{__('ui.catadsno')}}</h2>
-            <a class="btn btn-danger" href="{{route('createAd')}}">{{__('ui.catadsnew')}}</a>
-            @endforelse
+            <div class="col-12 d-flex flex-column align-items-center">
+                <h2 class="mt-5">{{__('ui.catadsno')}}</h2>
+                <a class="btn btn-newads mt-2 mb-5" href="{{route('createAd')}}">{{__('ui.catadsnew')}}</a>
+                <img src="/../media/categoryAds.png" alt="..." width="900px" height="700px" >
             </div>
+            @endforelse
         </div>
     </div>
-
+    
 </x-layout>
