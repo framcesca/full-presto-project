d4e473dedb01040dd69309282334ec3b7905e44f
marco <marco.cassarino@gmail.com>
2022-06-28 16:05:51 +0200

    Imported Google Vision API, Created job Google Vision Search

--- resources/views/mail/becomeRevisor.blade.php
+++ resources/views/mail/becomeRevisor.blade.php
@@ -15,7 +15,7 @@
         <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.log-mail')}}: {{$user->email}}</p>
         <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.desc')}}: {{$description}}</p>
         <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.renrev')}}</p>
-        <a style="background-color: white; color: black; padding:10px; border-radius:15px; font-size: 25px; border: 1px black; text-decoration:none;" href="{{route('createRevisor', $user)}}">{{__('ui.potere')}}</a>
+        <a style="background-color: white; color: black; padding:10px; font-size: 25px; border: 1px black; text-decoration:none;" href="{{route('createRevisor', $user)}}">{{__('ui.potere')}}</a>
       </div>
    </body>
 </html>
--- resources/views/mail/isRevisor.blade.php
+++ resources/views/mail/isRevisor.blade.php
@@ -8,11 +8,11 @@
 </head>
 <body style='background: url(https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80); background-repeat: no-repeat; background-position: center; background-size: auto;'>
   <div>
-      <h1 style="font-family:Segoe UI; color: White;text-align:center; margin-top: 50px; padding-start:10px; text-shadow: 3px 3px 3px black;">{{__('ui.congrats')}} {{$user->name}}{{__('ui.accettata')}}</h1>
+      <h1 style="font-family:Segoe UI; color: White;text-align:center; margin-top: 50px; padding-start:10px; text-shadow: 1px 1px 1px black;">{{__('ui.congrats')}} {{$user->name}}{{__('ui.accettata')}}</h1>
         <br>
-        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.selodicitu')}}: {{$user->email}}</p>
+        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 1px 1px 1px black;">{{__('ui.selodicitu')}}: {{$user->email}}</p>
         <br>
-        <button style="background-color: white; color: black; padding:10px; border-radius:15px; font-size: 25px; border: 2px black;" href="{{route('login')}}">{{__('ui.ecco')}}</button>
+        <a style="background-color: white; color: black; padding:10px; border-radius:15px; font-size: 25px; border: 2px black;" href="{{route('login')}}">{{__('ui.ecco')}}</a>
         {{-- <a href="{{route('login')}}">{{__('ui.ecco')}}</a> --}}
     </div>
 </body>
