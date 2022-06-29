fc837919b1b09b1964b33da967c9df9ac42a4b93
Tommaso Simone <tsimone9838@gmail.com>
2022-06-29 13:14:40 +0200

    Styled emails

--- resources/views/mail/becomeRevisor.blade.php
+++ resources/views/mail/becomeRevisor.blade.php
@@ -1,21 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{__('ui.titlee')}}</title>
-  
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>{{__('ui.titlee')}}</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <title>Simple Transactional Email</title>
+   <style>
+      
+      body{
+         font-family: Segoe UI;
+      }
+
+      .btn-email{
+         padding: 10px 15px;
+         border: none;
+         border-radius: 8px;
+         box-shadow: 10px 24px 54px rgba(32, 30, 52, 0.16);
+         background-color: rgb(249, 212, 1);
+         transition: 0.3s;
+         text-decoration: none;
+         color: black;
+         padding-top: 8px;
+      }
+      .btn-email:hover{
+         box-shadow: none;
+      }
+
+      .email-body{
+         display: flex;
+         align-items: center;
+         flex-direction: column;
+         margin-top: 60px; 
+         text-align: center;
+      }
+      .padding-email{
+         padding-top: 15px;
+      }
+
+
+      @media screen and (min-width: 768px){
+         .img-responsive{
+            height: 600px;
+            width: 800px;
+         }
+      }
+      @media screen and (max-width: 768px){
+         .img-responsive{
+            height: 500px;
+            width: 600px;
+         }
+      }
+      @media screen and (max-width: 480px){
+         .img-responsive{
+            height: 200px;
+            width: 270px;
+         }
+      }
+
+
+   </style>
 </head>
-   <body style='background: url(https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80); background-repeat: no-repeat; background-position: center; background-size: auto;'>
-     <div style="margin-top: 50px;">
-        <h1 style="font-family:Segoe UI; color: White;text-align:center;margin-top: 50px; padding-start:10px; text-shadow: 3px 3px 3px black;">{{__('ui.noii')}}</h1>
-        <h4 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.dati')}}</h2>
-        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.reg-name')}}: {{$user->name}}</p>
-        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.log-mail')}}: {{$user->email}}</p>
-        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.desc')}}: {{$description}}</p>
-        <h2 style="font-family:Segoe UI; color: White;text-align:center;padding: 10px; text-shadow: 3px 3px 3px black;">{{__('ui.renrev')}}</p>
-        <a style="background-color: white; color: black; padding:10px; font-size: 25px; border: 1px black; text-decoration:none;" href="{{route('createRevisor', $user)}}">{{__('ui.potere')}}</a>
+<body>
+   <div class="email-body">
+      <div>
+         <h1>{{__('ui.noii')}}</h1>
+         <h2 class="padding-email">{{__('ui.dati')}}</h2>
+         <p>{{__('ui.reg-name')}}: {{$user->name}}</p>
+         <p>{{__('ui.log-mail')}}: {{$user->email}}</p>
+         <p>{{__('ui.desc')}}: {{$description}}</p>
+         <h3 class="padding-email">{{__('ui.renrev')}}</h3>
+         <a class="btn-email" href="{{route('createRevisor', $user)}}">{{__('ui.potere')}}</a>
       </div>
-   </body>
-</html>
+      <div>
+         <img class="img-responsive" src="<?php echo $message->embed(public_path().'\media\email2.png'); ?>">
+      </div>
+   </div>
+   
+   
+</body>
